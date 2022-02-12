90da1506b18aacc7bc9e650a0dd6977deebbec9e
Hympert Nguyen <hnguyen013@alvamfg.local>
2022-02-11 16:58:29 -0800

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [searchString, setSearchString] = useState();
+	const [searchResult, setSearchResult] = useState();
+
+	useEffect(() => {}, []);
+
+	const FormSubmitHandler = (e) => {
+		e.preventDefault();
+
+		fetch(`http://localhost:4001/bookSearch`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ q: searchString }),
+		})
+			.then((result) => {
+				setSearchResult(result);
+			})
+			.catch((err) => console.log('Failed to send book search request to server', err));
+
+		setSearchString('');
+	};
+
+	return (
+		<div>
+			<h1>Reverie Library</h1>
+			<div style={{ width: '50vw', height: '200px', margin: '0 auto' }}>
+				<Form style={{ display: 'flex' }} onSubmit={FormSubmitHandler}>
+					<Form.Control
+						type='text'
+						placeholder='Search...'
+						value={searchString}
+						onChange={(e) => setSearchString(e.target.value)}
+					/>
+					<Button type='Submit'>Submit</Button>
+				</Form>
+			</div>
+
+			<div></div>
+			{console.log(searchResult)}
+		</div>
+	);
 }
 
 export default App;
