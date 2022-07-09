c9ec4d9c9e0505b350e51d2eb819dec1395455f8
Hympert Nguyen <fieridneil@gmail.com>
2022-07-09 00:36:29 -0700

    commit

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const app = express();
 const port = process.env.PORT || 4001;
 const csv = require("fast-csv");
 const cors = require("cors");
+const path = require("path");
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 const getDataset = async () => {
 	const records = [];
